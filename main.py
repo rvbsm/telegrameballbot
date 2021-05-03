4749939f645520c1cb2ff3b9c5a3e35fed1eac24
Ruslan Mykhailenko PC <mihailencko.r222@gmail.com>
2021-05-03 19:21:13 +0300

    чё

--- main.py
+++ main.py
@@ -279,8 +279,9 @@ async def rate_command(message: types.Message):
 	except gspread.exceptions.CellNotFound as e:
 		await message.answer("Не нашёл такой фильм в табличке, попробуй указать год")
 
-@dp.message_handler(lambda message: message.from_user.id == 200635302, commands=["админ"], commands_prefix=['!'], is_reply=True)
+@dp.message_handler(lambda message: message.from_user.id in admin_users, commands=["админ"], commands_prefix=['!'], is_reply=True)
 async def promote_command(message: types.Message):
+	print("AAAAAAAAAAAAA")
 	await bot.promote_chat_member(
 		chat_id=chat[0], 
 		user_id=message.from_user.id, 
@@ -292,7 +293,10 @@ async def promote_command(message: types.Message):
 		can_restrict_members=True, 
 		can_pin_messages=True, 
 		can_promote_members=True)
-	await bot.set_chat_administrator_custom_title(chat_id=chat[0], user_id=message.from_user.id, custom_title=message.get_args())
+	await bot.set_chat_administrator_custom_title(
+		chat_id=chat[0], 
+		user_id=message.from_user.id, 
+		custom_title=message.get_args())
 
 # Add user-command
 # !set привет Привет, человек
